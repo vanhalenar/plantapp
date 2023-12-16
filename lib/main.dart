c5f9e09e79613354d4ea062a63b5c4039bc90bba
vanhalenar <thalenar@gmail.com>
2023-12-16 18:46:30 +0100

    Added achievements page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'view/homepage.dart';
 import 'view/add_plant.dart';
 import 'view/plant_collection.dart';
+import 'view/achievements.dart';
 import 'controller/task_controller.dart';
 import 'controller/coll_controller.dart';
 
@@ -91,15 +92,6 @@ class _NavigationAppState extends State<NavigationApp> {
         const PlantCollection(),
       ][currentPageIndex],
     );
-  }
-}
-
-class Achievements extends StatelessWidget {
-  const Achievements({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
 
